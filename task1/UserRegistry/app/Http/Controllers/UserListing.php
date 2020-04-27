2ebc761b262eb93eff580017fd62513500988fe4
Stephan Van Zyl <stephan@flickerleap.com>
2020-04-27 12:38:11 +0200

    Complete task2/Everlytic Developer Assessment.docx

--- task1/UserRegistry/app/Http/Controllers/UserListing.php
+++ task1/UserRegistry/app/Http/Controllers/UserListing.php
@@ -34,32 +34,9 @@ class UserListing extends Controller
      */
     public function index()
     {
-        for ($i = 1; $i <= 100; $i++) {
-            $multipleOfThree = (($i % 3) === 0);
-            $multipleOfFive = (($i % 5) === 0);
-
-            if (!$multipleOfThree && !$multipleOfFive) {
-                print $i . "<br/>";
-
-                // No need to evaluate further
-                continue;
-            }
-
-            if ($multipleOfThree) {
-                print('Fizz');
-            }
-
-            if ($multipleOfFive) {
-                print('Buzz');
-            }
-
-            print("<br/>");
-        }
-
-
-        // $users = $this->user->paginate(10);
+        $users = $this->user->paginate(10);
         
-        // return view('users.index', compact('users'));
+        return view('users.index', compact('users'));
     }
 
     /**
